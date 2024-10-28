f34a7b9a13b093024889b289d52e71055dcccdce
Kreasyy <erkolismail74@gmail.com>
2024-10-28 13:04:50 +0300

    Update main.lua

--- kreasyeasyitems/main.lua
+++ kreasyeasyitems/main.lua
@@ -1,122 +1,82 @@
---Kreasy Trafından Yapılmıştır
+-- Kreasy Tarafından Yapılmıştır
 local MyMod = RegisterMod("HealthHeartsOnKeyPress", 1)
 
 local cooldown = 0.43  -- 1 saniyelik cooldown süresi
-local lastPressRed = 0  -- Son kırmızı kalp basım zamanı
-local lastPressSoul = 0  -- Son ruh kalbi basım zamanı
+local lastPressRed = 0       -- Son kırmızı kalp basım zamanı
+local lastPressSoul = 0      -- Son ruh kalbi basım zamanı
+local lastPressBlack = 0     -- Son siyah kalp basım zamanı
+local lastPressCoin = 0      -- Son para basım zamanı
+local lastPressBomb = 0      -- Son bomba basım zamanı
+local lastPressKey = 0       -- Son anahtar basım zamanı
+local lastPressContainer = 0 -- Son kalp konteyneri basım zamanı
 
--- Tuşlara basıldığında kalp eklenmesi
+-- Tuşlara basıldığında item eklenmesi
 function MyMod:onUpdate()
     local player = Isaac.GetPlayer(0)  -- İlk oyuncuyu al
     local currentTime = Isaac.GetFrameCount() / 30  -- Geçerli zamanı saniye cinsinden al
 
-    -- I tuşuna basıldıysa (Kırmızı kalp için)
+    -- 1 tuşuna basıldıysa (Kırmızı kalp için)
     if Input.IsButtonPressed(Keyboard.KEY_1, player.ControllerIndex) then
-        -- Kırmızı kalp cooldown kontrolü
         if currentTime - lastPressRed >= cooldown then
-            local currentHearts = player:GetHearts()  -- Mevcut kırmızı kalp sayısını al
-            local maxHearts = player:GetMaxHearts()  -- Maksimum kırmızı kalp sayısını al
-            
-            -- Eğer mevcut kalp sayısı maksimumdan düşükse
+            local currentHearts = player:GetHearts()
+            local maxHearts = player:GetMaxHearts()
             if currentHearts < maxHearts then
-                player:AddHearts(1)  -- 1 tane kırmızı kalp ekle
+                player:AddHearts(1)
             else
-                print("Maksimum kalp sayısına ulaşıldı.")  -- Konsola mesaj yazdır
+                print("Maksimum kalp sayısına ulaşıldı.")
             end
-            
-            lastPressRed = currentTime  -- Son basım zamanını güncelle
+            lastPressRed = currentTime
         end
     end
 
     -- 2 tuşuna basıldığında (Ruh kalbi için)
     if Input.IsButtonPressed(Keyboard.KEY_2, player.ControllerIndex) then
-        -- Ruh kalbi cooldown kontrolü
         if currentTime - lastPressSoul >= cooldown then
-            player:AddSoulHearts(1)  -- 2 tane ruh kalbi ekle
-            lastPressSoul = currentTime  -- Son basım zamanını güncelle
+            player:AddSoulHearts(1)
+            lastPressSoul = currentTime
         end
     end
-end
 
--- Modun her güncellemede çalışmasını sağla
-MyMod:AddCallback(ModCallbacks.MC_POST_UPDATE, MyMod.onUpdate)
-
-
-local MyMod = RegisterMod("FullHealthOnKPress", 1)
-
-local cooldown = 0.40  -- 1 saniyelik cooldown süresi
-local lastPress = 0  -- Son basım zamanı
-
--- K tuşuna basıldığında oyuncunun para kazanması
-function MyMod:onUpdate()
-    local player = Isaac.GetPlayer(0)  -- İlk oyuncuyu al
-    local currentTime = Isaac.GetFrameCount() / 30  -- Geçerli zamanı saniye cinsinden al
-
-    -- N tuşuna basıldıysa
-    if Input.IsButtonPressed(Keyboard.KEY_4, player.ControllerIndex) then
-        -- Cooldown kontrolü
-        if currentTime - lastPress >= cooldown then
-            player:AddCoins(1)  -- 1 para ekle
-            lastPress = currentTime  -- Son basım zamanını güncelle
-        end
-    end
-end
-
--- Modun her güncellemede çalışmasını sağla
-MyMod:AddCallback(ModCallbacks.MC_POST_UPDATE, MyMod.onUpdate)
-
-local MyMod = RegisterMod("FullHealthOnKPress", 1)
-
-local cooldown = 0.40  -- 1 saniyelik cooldown süresi
-local lastPress = 0  -- Son basım zamanı
-
--- I tuşuna basıldığında oyuncuya siyah kalp eklenmesi
-function MyMod:onUpdate()
-    local player = Isaac.GetPlayer(0)  -- İlk oyuncuyu al
-    local currentTime = Isaac.GetFrameCount() / 30  -- Geçerli zamanı saniye cinsinden al
-
-    -- I tuşuna basıldıysa
+    -- 3 tuşuna basıldığında (Siyah kalp için)
     if Input.IsButtonPressed(Keyboard.KEY_3, player.ControllerIndex) then
-        -- Cooldown kontrolü
-        if currentTime - lastPress >= cooldown then
-            player:AddBlackHearts(1)  -- 1 tane siyah kalp ekle
-            lastPress = currentTime  -- Son basım zamanını güncelle
+        if currentTime - lastPressBlack >= cooldown then
+            player:AddBlackHearts(1)
+            lastPressBlack = currentTime
         end
     end
-end
 
--- Modun her güncellemede çalışmasını sağla
-MyMod:AddCallback(ModCallbacks.MC_POST_UPDATE, MyMod.onUpdate)
+    -- 4 tuşuna basıldığında (Para için)
+    if Input.IsButtonPressed(Keyboard.KEY_4, player.ControllerIndex) then
+        if currentTime - lastPressCoin >= cooldown then
+            player:AddCoins(1)
+            lastPressCoin = currentTime
+        end
+    end
 
-local MyItemsMod = RegisterMod("FullHealthOnKPress", 1)
-
-local cooldown = 0.45  -- 1 saniyelik cooldown süresi
-local lastPressBomb = 0  -- Son bomba basım zamanı
-local lastPressKey = 0  -- Son anahtar basım zamanı
-
--- Tuşlara basıldığında nesne eklenmesi
-function MyItemsMod:onUpdate()
-    local player = Isaac.GetPlayer(0)  -- İlk oyuncuyu al
-    local currentTime = Isaac.GetFrameCount() / 30  -- Geçerli zamanı saniye cinsinden al
-
-    -- 5 tuşuna basıldıysa (Bomba için)
+    -- 5 tuşuna basıldığında (Bomba için)
     if Input.IsButtonPressed(Keyboard.KEY_5, player.ControllerIndex) then
-        -- Bomba cooldown kontrolü
         if currentTime - lastPressBomb >= cooldown then
-            player:AddBombs(1)  -- 1 tane bomba ekle
-            lastPressBomb = currentTime  -- Son basım zamanını güncelle
+            player:AddBombs(1)
+            lastPressBomb = currentTime
         end
     end
 
     -- 6 tuşuna basıldığında (Anahtar için)
     if Input.IsButtonPressed(Keyboard.KEY_6, player.ControllerIndex) then
-        -- Anahtar cooldown kontrolü
         if currentTime - lastPressKey >= cooldown then
-            player:AddKeys(1)  -- 1 tane anahtar ekle
-            lastPressKey = currentTime  -- Son basım zamanını güncelle
+            player:AddKeys(1)
+            lastPressKey = currentTime
+        end
+    end
+
+    -- 7 tuşuna basıldığında (Kalp konteyneri için)
+    if Input.IsButtonPressed(Keyboard.KEY_7, player.ControllerIndex) then
+        if currentTime - lastPressContainer >= cooldown then
+            player:AddMaxHearts(2, false)  -- 1 tam kalp konteyneri ekle (2 yarım kalp)
+            lastPressContainer = currentTime
         end
     end
 end
 
 -- Modun her güncellemede çalışmasını sağla
-MyItemsMod:AddCallback(ModCallbacks.MC_POST_UPDATE, MyItemsMod.onUpdate)
+MyMod:AddCallback(ModCallbacks.MC_POST_UPDATE, MyMod.onUpdate)
